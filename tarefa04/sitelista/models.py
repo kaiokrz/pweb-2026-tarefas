8553eb65cc57748bacc10f5ca317b4b9238ec1c7
Kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-06-24 10:20:22 -0300

    dando errado

--- tarefa04/sitelista/models.py
+++ tarefa04/sitelista/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
-class Tarefas(models.Model):
+class Tarefa(models.Model):
     nome = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     prazo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+    def atraso(self, data):
+        if(self.prazo<data):
+            return "Atrasado"
--- tarefa04/sitelista/views.py
+++ tarefa04/sitelista/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from datetime import date
+from sitelista.models import Tarefa
 
 def index(request):
-    context = {'hoje':date.today().strftime('%d/%m/%Y')}
+    context = {'tarefas':Tarefa.objects.all(),'hoje':date.today().strftime('%d/%m/%Y')}
     return render(request, "index.html", context)
