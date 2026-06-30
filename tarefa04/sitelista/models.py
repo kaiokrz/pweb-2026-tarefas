8fa77b89c2c0f6941ee68815e7568aa94add8d80
kaio luiz <kaio.cruz@escolar.ifrn.edu.br>
2026-06-30 15:51:40 -0300

    mais uns negocio

--- tarefa04/sitelista/models.py
+++ tarefa04/sitelista/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class Tarefa(models.Model):
     nome = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
-    prazo = models.CharField(max_length=100)
-
+    status = models.BooleanField(verbose_name="Concluído")
+    prazo = models.DateField()
     def __str__(self):
         return self.nome
     
