16a5c32f5cb41e1612d1ba8f8b60e391271a8aa5
Kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-07-01 09:04:31 -0300

    tarefa04 finalizada

--- tarefa04/sitelista/models.py
+++ tarefa04/sitelista/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 
 class Tarefa(models.Model):
     nome = models.CharField(max_length=100)
--- tarefa04/sitelista/views.py
+++ tarefa04/sitelista/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from datetime import date
 from sitelista.models import Tarefa
 
+tarefas = Tarefa.objects.all()
+hoje = date.today()
+
 def index(request):
-    context = {'tarefas':Tarefa.objects.all(),'hoje':date.today().strftime('%d/%m/%Y')}
+    context = {'tarefas':tarefas,'hoje':hoje}
     return render(request, "index.html", context)
