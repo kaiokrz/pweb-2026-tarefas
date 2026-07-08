d77d4833b3a0024f88b525d8794616d57dacc35f
Kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-07-08 10:20:16 -0300

    ajeitando

--- tarefa05/blog/views.py
+++ tarefa05/blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def index(request):
-
-    return render(request, "blog/index.html")
+    context = {"postagis": Post.objects.all(),}
+    return render(request, "blog/index.html", context) 
 
 def post(request, post_id):
-    context = {}
+    context = {"post": get_object_or_404(Post, id=post_id),}
     return render(request, "blog/post.html", context)
-    
