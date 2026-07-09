57109b806b6b4befbdfe07b9a8834b69362683d1
Kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-07-09 11:16:04 -0300

    tarefa05 finalizada

--- tarefa05/blog/models.py
+++ tarefa05/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     texto = models.TextField()
-    foto = models.ImageField(upload_to=("imgs"))
+    foto = models.ImageField(upload_to=('imgs/'))
     data = models.DateField(auto_now=True)
 
     def __str__(self):
